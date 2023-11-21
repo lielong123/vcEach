453a9b4db93716982d9c5b60996531fbb861e122
lielong123 <jingfeng20958069@126.com>
2023-11-21 12:42:55 -0800

    Setup FreeRTOS

--- src/main.cpp
+++ src/main.cpp
@@ -1,77 +1,72 @@
+
 #include <stdio.h>
 #include "pico/stdlib.h"
-#include "hardware/pio.h"
+#include "pico/multicore.h"
 #include "pico/cyw43_arch.h"
-#include "hardware/uart.h"
+#include "FreeRTOS.h"
+#include "task.h"
 
-#include "blink.pio.h"
 
-void blink_pin_forever(PIO pio, uint sm, uint offset, uint pin, uint freq) {
-    blink_program_init(pio, sm, offset, pin);
-    pio_sm_set_enabled(pio, sm, true);
+static void blinkTask(void *pvParameters) {
+    (void) pvParameters; // unused parameter
 
-    printf("Blinking pin %d at %d Hz\n", pin, freq);
+    vTaskDelay(100);
 
-    // PIO counter program takes 3 more cycles in total than we pass as
-    // input (wait for n + 1; mov; jmp)
-    pio->txf[sm] = (125000000 / (2 * freq)) - 3;
+    while (1) {
+        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 1);
+        vTaskDelay(pdMS_TO_TICKS(1000));
+        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 0);
+        vTaskDelay(pdMS_TO_TICKS(1000));
+    }
 }
 
 
-// UART defines
-// By default the stdout UART is `uart0`, so we will use the second one
-#define UART_ID uart1
-#define BAUD_RATE 115200
+static void printTask(void *pvParameters) {
+    (void) pvParameters; // unused parameter
 
-// Use pins 4 and 5 for UART1
-// Pins can be changed, see the GPIO function select table in the datasheet for information on GPIO assignments
-#define UART_TX_PIN 4
-#define UART_RX_PIN 5
+    while (1) {
+        printf("Hello from FreeRTOS task!\n");
+        vTaskDelay(pdMS_TO_TICKS(1000));
+    }
+}
 
-
-
-int main()
-{
+int main(void) {
     stdio_init_all();
 
-    // Initialise the Wi-Fi chip
-    if (cyw43_arch_init()) {
-        printf("Wi-Fi init failed\n");
-        return -1;
-    }
+    printf("FreeRTOS on RP2040\n");
+    cyw43_arch_init();
 
-    // PIO Blinking example
-    PIO pio = pio0;
-    uint offset = pio_add_program(pio, &blink_program);
-    printf("Loaded program at %d\n", offset);
-    
-    #ifdef PICO_DEFAULT_LED_PIN
-    blink_pin_forever(pio, 0, offset, PICO_DEFAULT_LED_PIN, 3);
-    #else
-    blink_pin_forever(pio, 0, offset, 6, 3);
-    #endif
-    // For more pio examples see https://github.com/raspberrypi/pico-examples/tree/master/pio
+    static TaskHandle_t blinkTaskHandle;
+    static TaskHandle_t printTaskHandle;
 
-    // Example to turn on the Pico W LED
-    cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 1);
+    xTaskCreate(blinkTask, "Blink", 256, nullptr, 1, &blinkTaskHandle);
+    xTaskCreate(printTask, "Print", 256, nullptr, 1, &printTaskHandle);
 
-    // Set up our UART
-    uart_init(UART_ID, BAUD_RATE);
-    // Set the TX and RX pins by using the function select on the GPIO
-    // Set datasheet for more information on function select
-    gpio_set_function(UART_TX_PIN, GPIO_FUNC_UART);
-    gpio_set_function(UART_RX_PIN, GPIO_FUNC_UART);
-    
-    // Use some the various UART functions to send out data
-    // In a default system, printf will also output via the default UART
-    
-    // Send out a string, with CR/LF conversions
-    uart_puts(UART_ID, " Hello, UART!\n");
-    
-    // For more examples of UART use see https://github.com/raspberrypi/pico-examples/tree/master/uart
+    vTaskCoreAffinitySet(blinkTaskHandle, 0x01); // Set the task to run on core 1
+    vTaskCoreAffinitySet(printTaskHandle, 0x02); // Set the task to run on core 2
 
-    while (true) {
-        printf("Hello, world2!\n");
-        sleep_ms(1000);
-    }
+    vTaskStartScheduler();
+
+    return 0;
 }
+
+void vApplicationMallocFailedHook() { configASSERT((volatile void*)nullptr); }
+
+void vApplicationIdleHook() {
+    volatile size_t xFreeHeapSpace;
+
+    xFreeHeapSpace = xPortGetFreeHeapSize();
+
+    (void)xFreeHeapSpace;
+}
+
+#if (configCHECK_FOR_STACK_OVERFLOW > 0)
+
+void vApplicationStackOverflowHook(TaskHandle_t xTask, char* pcTaskName) {
+    /* Check pcTaskName for the name of the offending task,
+     * or pxCurrentTCB if pcTaskName has itself been corrupted. */
+    (void)xTask;
+    (void)pcTaskName;
+}
+
+#endif /* #if ( configCHECK_FOR_STACK_OVERFLOW > 0 ) */
