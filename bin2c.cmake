383a96cceeb2a40a0fba8d5d51aa87bc645fc871
lielong123 <jingfeng20958069@126.com>
2024-11-23 18:20:16 -0800

    Bin2c.cmake: Trick the linker into showing the actual used size of the file-system partition

--- bin2c.cmake
+++ bin2c.cmake
@@ -1,5 +1,29 @@
 file(READ ${BIN_FILE} CONTENTS HEX)
-string(REGEX REPLACE "([0-9a-f][0-9a-f])" "0x\\1, " CONTENTS ${CONTENTS})
+
+string(LENGTH "${CONTENTS}" CONTENTS_LENGTH)
+math(EXPR MAX_POS "${CONTENTS_LENGTH} - 2")
+set(LAST_NON_FF_POS -1)
+
+foreach(POS RANGE 0 ${MAX_POS} 2)
+    string(SUBSTRING "${CONTENTS}" ${POS} 2 BYTE)
+
+    if(NOT "${BYTE}" STREQUAL "ff")
+        set(LAST_NON_FF_POS ${POS})
+    endif()
+endforeach()
+
+if(${LAST_NON_FF_POS} GREATER -1)
+    math(EXPR TRUNC_POS "${LAST_NON_FF_POS} + 2")
+    string(SUBSTRING "${CONTENTS}" 0 ${TRUNC_POS} TRUNCATED)
+
+    math(EXPR TRUNCATED_SIZE "${TRUNC_POS} / 2")
+else()
+    set(TRUNCATED "")
+    set(TRUNCATED_SIZE 0)
+endif()
+
+string(REGEX REPLACE "([0-9a-f][0-9a-f])" "0x\\1, " C_CONTENT ${TRUNCATED})
 file(WRITE ${C_FILE} "/* Auto-generated filesystem data */\n")
-file(APPEND ${C_FILE} "const unsigned char __attribute__((section(\".littlefs_fs\"))) fs_data[] = {\n  ${CONTENTS}\n};\n\n")
+file(APPEND ${C_FILE} "const unsigned char __attribute__((section(\".littlefs_fs\"))) fs_data[] = {\n  ${C_CONTENT}\n};\n\n")
 file(APPEND ${C_FILE} "const unsigned int fs_data_size = ${BIN_SIZE};\n")
+file(APPEND ${C_FILE} "const unsigned int fs_data_actual_size = ${TRUNCATED_SIZE};\n")
